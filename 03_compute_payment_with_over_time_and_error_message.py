2076fbcebfcded37e4ee3221fd725cceb701c5d7
rh13 <rhlikhon13@gmail.com>
2018-03-28 15:36:04 +0600

    solved compute payment with overtime and error message

--- 03_compute_payment_with_over_time_and_error_message.py
+++ 03_compute_payment_with_over_time_and_error_message.py
@@ -1,29 +1,18 @@
-"""
-Write a program that will take hours and rate per hour as input and will compute the total payment.  your program will give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+while True:
 
-Here your program will show an error message for non-numeric value of hours/rate. After showing the error message, the program will exit
+ hours = input('Enter Hours: ')
+ rate = input('Enter Rate:')
 
-Example 01:
-Enter Hours: 45
-Enter Rate: 10
-Pay: 475.00
+ try:
+    hours = int(hours)
+    rate = int(rate)
+ except :
+    print ('Error, please enter numeric input')
+    break
+        
 
+ pay=hours*rate 
+ if hours>40:
+  pay=40*rate+(hours-40)*rate*1.5
 
-Example 02:
-Enter Hours: 45
-Enter Rate: nine
-Error, please enter numeric input
-
-"""
-
-
-# your code here
-
-
-# take hours and rate as input
-hours = int(input('Enter Hours: '))
-rate = float(input('Enter Rate:'))
-pay = 0
-
-
-print('Pay: {:.2f}'.format(pay))
+ print('Pay: {:.2f}'.format(pay))

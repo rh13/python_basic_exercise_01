1eb26467be5a4cc4d16ea8fc7da13e1ab90404f7
rh13 <rhlikhon13@gmail.com>
2018-03-28 15:38:00 +0600

    solved compute payment with over or leavetime and error message in function

--- 05_compute_payment_with_over_or_leave_time_and_error_message_in_function.py
+++ 05_compute_payment_with_over_or_leave_time_and_error_message_in_function.py
@@ -1,42 +1,24 @@
-"""
-Write a function named computepay that will take hours and rate per hour as input and will compute the total payment.  your program will give the employee 1.5 times the hourly rate for hours worked above 40 hours and will deduct the .5 times the hourly rate for hours less than 40.
 
-Here your program will show an error message for non-numeric value of hours/rate. After showing the error message, the program will exit
+while True:
 
-Example 01:
-Enter Hours: 45
-Enter Rate: 10
-Pay: 475.00
+ hours = input('Enter Hours: ')
+ rate = input('Enter Rate:')
 
-Example 02:
-Enter Hours: 40
-Enter Rate: 10
-Pay: 400.00
+ try:
+    hours = int(hours)
+    rate = int(rate)
+ except :
+    print ('Error, please enter numeric input')
+    break
+ 
+ def computepay(hours, rate):
+  if hours<40:
+   pay=hours*rate-(40-hours)*rate*.5
+  else:
+   pay=40*rate+(hours-40)*rate*1.5  
+  print('pay: {:.2f}'.format(pay))
 
-Example 03:
-Enter Hours: 39
-Enter Rate: 10
-Pay: 385
-
-Example 04:
-Enter Hours: 45
-Enter Rate: nine
-Error, please enter numeric input
-
-"""
-
-
-def computepay(hours, rate):
-    pay = 0
-    # your code here
-
-    return pay
-
-
-# take hours and rate as input
-hours = int(input('Enter Hours: '))
-rate = float(input('Enter Rate:'))
-pay = computepay(hours, rate)
-
-if pay:
-    print('Pay: {:.2f}'.format())
+  
+ pay = computepay(hours, rate)
+ 
+ 
